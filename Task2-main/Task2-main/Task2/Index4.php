4cfa4cef0fb37722726c7212b18b0d27e31c25a9
Ela <98597693+ElaGitNET@users.noreply.github.com>
2023-02-01 11:56:46 +0000

    Add files via upload

--- Task2-main/Task2-main/Task2/Index4.php
+++ Task2-main/Task2-main/Task2/Index4.php
@@ -34,8 +34,9 @@
             position:relative;
         }
         </style>
-
-                <form method="post">
+                <br>
+                <br>
+                <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
                 Gender:
                 <Select name="gender" ><br>
                     <option>Male</option>
@@ -50,23 +51,25 @@
                     <br><input type="Submit" value="Calculate">
 
                     <?php
-                        $age=$_POST['age'];
-                        $weight=$_POST['weight'];
-                        $height = $_POST['height'];
-                        $calories="0.0215183";
-                        $gender=$_POST['gender'];
+                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                            $age=$_POST['age'];
+                            $weight=$_POST['weight'];
+                            $height = $_POST['height'];
+                            $calories="0.0215183";
+                            $gender=$_POST['gender'];
 
-                            switch($gender){
-                                case 'Female':
-                                    $gender= 665 + (9.6 * $weight) + (1.8 * $height) - (4.7 * $age);
-                                    echo "<p>Your estimated daily metabolic rate is $gender </p>";
-                                    echo "<p>This means that you need rouhgly $gender calories a day to maintain your current weight.</p>";
-
-                                    break;
-                                    case 'Male':
-                                        $gender=66 + (13.7 *$weight) + (5 * $height) - (6.8 * $age);
+                                switch($gender){
+                                    case 'Female':
+                                        $gender= 665 + (9.6 * $weight) + (1.8 * $height) - (4.7 * $age);
                                         echo "<p>Your estimated daily metabolic rate is $gender </p>";
                                         echo "<p>This means that you need rouhgly $gender calories a day to maintain your current weight.</p>";
+
+                                        break;
+                                        case 'Male':
+                                            $gender=66 + (13.7 *$weight) + (5 * $height) - (6.8 * $age);
+                                            echo "<p>Your estimated daily metabolic rate is $gender </p>";
+                                            echo "<p>This means that you need rouhgly $gender calories a day to maintain your current weight.</p>";
+                                }
                             }
             ?>
                 </form>
